841494d0aea255e705386a3794693f927895f7f6
baokimho <kim.ho@edu.turkuamk.fi>
2026-03-07 11:42:43 +0700

    logging middlware

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -4,6 +4,11 @@ import routes from "./routes";
 const app = express();
 
 app.use(express.json());
+// logging middleware
+app.use((req, _res, next) => {
+  console.log(`[Gateway] ${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/api", routes);
 
