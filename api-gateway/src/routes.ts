4aa8a8839af170e923b2fb1eef5ccafb0e3df53d
baokimho <kim.ho@edu.turkuamk.fi>
2026-03-17 13:41:19 +0700

    set up sql and migrate prisma

--- api-gateway/src/routes.ts
+++ api-gateway/src/routes.ts
@@ -2,28 +2,19 @@ import express from "express";
 import { jwtAuth } from "./middleware/jwtAuth.middleware";
 import { pasetoAuth } from "./middleware/pasetoAuth.middleware";
 import { resourceProxy } from "./proxy/resource.proxy";
+import {
+  getJwtProtected,
+  getPasetoProtected,
+  getPublic,
+} from "./controllers/gateway.controller";
 
 
 const router = express.Router();
 
-router.get("/public", (_req, res) => {
-  res.json({
-    message: "Public endpoint",
-  });
-});
+router.get("/public", getPublic);
 
-router.get("/jwt-protected", jwtAuth, (req, res) => {
-  res.json({
-    message: "Protected endpoint by JWT",
-    user: req.user,
-  });
-});
-router.get("/paseto-protected", pasetoAuth, (req, res) => {
-  res.json({
-    message: "Protected endpoint by PASETO",
-    user: req.user,
-  });
-});
+router.get("/jwt-protected", jwtAuth, getJwtProtected);
+router.get("/paseto-protected", pasetoAuth, getPasetoProtected);
 
 router.use("/jwt-resource", jwtAuth, resourceProxy);
 
